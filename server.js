ea85a1a1da01908af166111e5b8a805bdf65745e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-05 22:08:37 +0800

    refactor

--- server.js
+++ server.js
@@ -1,65 +1,68 @@
 // INITIALIZATION
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // DB CONNECT
-const mongoose = require("mongoose");
-const { mongoURI } = require("./configs/dbSecretKeys");
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const { mongoURI } = require('./configs/dbSecretKeys')
+
 const connectToDatabase = () => {
     try {
         mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
-        console.log("Connected to the DB");
-    } catch (err) {
-        console.log("Error connecting to the DB:", err);
+        })
+        console.log('Connected to the DB')
+    } catch (error) {
+        throw new Error(`Error connecting to the DB: ${error}`)
     }
-};
-connectToDatabase();
+}
+connectToDatabase()
 
 // MIDDLEWARE
-const cors = require('cors');
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const cors = require('cors')
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // ROUTES
-const auth = require('./routes/api/auth');
-const users = require('./routes/api/users');
-app.use('/api/auth', auth);
-app.use('/api/users', users);
+const auth = require('./routes/api/auth')
+const users = require('./routes/api/users')
+app.use('/api/auth', auth)
+app.use('/api/users', users)
 
 // SESSION
-const session = require('express-session');
-const sk = require('./configs/dbSecretKeys').secretOrKey;
+const session = require('express-session')
+const sk = require('./configs/dbSecretKeys').secretOrKey
 app.use(
     session({
         secret: sk,
         resave: true,
         saveUninitialized: true,
     })
-);
+)
 
 // PASSPORT
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-require('./configs/passport')(passport);
+const passport = require('passport')
+app.use(passport.initialize())
+app.use(passport.session())
+require('./configs/passport')(passport)
 
 // SET PORT, and START SERVER
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5100
 const server = () => {
     try {
-        app.listen(port);
-        console.log(`We are live at: ${port}`);
+        app.listen(port)
+        console.log(`We are live at: ${port}`)
     } catch (err) {
-        console.log(`Server startup failed: ${err}`);
+        console.log(`Server startup failed: ${err}`)
     }
-};
-server();
+}
+server()
 
 module.exports = {
     app,
     server,
-};
+}
